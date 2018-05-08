9617926d0385913133556e5515f22763dc367099
josep <jssdlcu1331@gmail.com>
2018-05-08 17:34:11 +0200

    initial commit

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+	bool MoveX = true;
+	float distance = 4.0f;
+	float direction = 1.0f;
+	public float speed = 4.0f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		if (MoveX) {
+			pos.x += Time.deltaTime * speed * direction;
+		if (pos.x >= distance) {
+			pos.x = distance;
+			direction = -direction;
+		}else if (pos.x <= -distance){
+			pos.x = -distance;
+			direction = -direction;
+		}
+	}
+	else {
+		pos.z += Time.deltaTime * speed * direction;
+		if (pos.z >= distance) {
+			pos.z = distance;
+			direction = -direction;
+		}else if (pos.z <= -distance){
+			pos.z = -distance;
+			direction = -direction;
+		}
+	}
+	transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/InstantiateController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateController : MonoBehaviour {
+	float x;
+	public GameObject cube; 
+	
+	public List<GameObject> j;
+	public GameObject s; 
+	float k = 0f;
+	float m = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x += Time.deltaTime;
+		
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			j.Add(cube);
+			GameObject d = Instantiate(s);
+			d.transform.position = new Vector3(j[j.Count-1].transform.position.x,j[j.Count-1].transform.position.y+m,j[j.Count-1].transform.position.z);
+			j.Add(d);
+			x = 0;
+			m+=1;
+		}
+
+	}
+}
